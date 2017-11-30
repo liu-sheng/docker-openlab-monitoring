75706c5cede9d12831794f4a978de0d66ac27481
liusheng <liusheng@huawei.com>
2017-11-30 20:42:08 +0800

    update

--- openlab_misc.sh
+++ openlab_misc.sh
@@ -26,7 +26,7 @@ chown zuul.zuul /srv/static/logs/ -R
 
 # Precondition
 apt-get update -y && apt-get upgrade -y
-apt-get install git python python-pip python3 python3-pip default-jdk python-psycopg2 -y
+apt-get install git python python-pip python3 python3-pip default-jdk python-psycopg2 curl -y
 
 # Install apache2
 apt-get install apache2 -y
@@ -60,10 +60,10 @@ graphite-manage syncdb --noinput
 # Install grafana
 cd ~
 wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.5.2_amd64.deb
-sudo apt-get install -y adduser libfontconfig
-sudo dpkg -i grafana_4.5.2_amd64.deb
+apt-get install -y adduser libfontconfig
+dpkg -i grafana_4.5.2_amd64.deb
 cp $cdir/conf/grafana/* /etc/grafana/
-#service grafana-server restart
+service grafana-server restart
 
 # install statsd
 apt-get install git nodejs devscripts debhelper dh-systemd -y
@@ -73,12 +73,13 @@ cd ~/build
 git clone https://github.com/etsy/statsd.git
 cd statsd
 dpkg-buildpackage
-cd .. 
+cd ..
+mkdir -p /etc/statsd/
 cp $cdir/conf/statsd/* /etc/statsd/
-sudo service carbon-cache stop
-sudo dpkg -i statsd*.deb
-#service carbon-cache start
-#service statsd restart
+service carbon-cache stop
+dpkg -i statsd*.deb
+service carbon-cache start
+service statsd restart
 
 # Install zuul status
 cp $cdir/conf/zuul/zuul.conf /etc/apache2/sites-available/
@@ -89,10 +90,10 @@ mkdir -p /var/lib/zuul/www
 cp -r $cdir/zuul-repo/etc/status/public_html/* /var/lib/zuul/www/
 #htpasswd -cbB /etc/apache2/grafana_htpasswd openlab openlab
 
-#sudo service carbon-cache stop
-#service carbon-cache start
-#service statsd restart
-#service grafana-server restart
+service carbon-cache stop
+service carbon-cache start
+service statsd restart
+service grafana-server restart
 
 # Configure apache security
 DEBIAN_FRONTEND=noninteractive apt-get install libapache2-mod-evasive libapache2-modsecurity -y
@@ -106,7 +107,7 @@ cp $cdir/conf/mod_evasive/evasive.conf /etc/apache/mods-available/
 apt-get install fail2ban -y
 cp $cdir/conf/fail2ban/jail.local /etc/fail2ban/jail.local
 cp $cdir/conf/fail2ban/apache-modsecurity.conf /etc/fail2ban/filter.d/
-#service fail2ban restart
+service fail2ban restart
 
 a2dissite 000-default
 a2ensite apache2-graphite
@@ -116,4 +117,4 @@ a2enmod proxy_http
 a2enmod ssl
 a2enmod xml2enc
 a2enmod rewrite
-#service apache2 restart
+service apache2 restart
